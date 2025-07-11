29a70843f500000199632a9b7918f5b37ff8a269
Will <wtdavis@wesleyan.edu>
2025-07-10 23:44:55 -0400

    test vite react useState

--- src/List.jsx
+++ src/List.jsx
@@ -1,1 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
+
+function List () {
+    
+    // fetch list of items
+
+    // sort list by relevance/ selected field/ visible
+
+    // turn list into <li> under a <ul>
+
+    let listArr = ["thing one", "thing two", "thing three", "thing four"]
+    
+
+
+    return (
+        <>
+            
+            
+
+        </>
+
+    )
+
+}
+
