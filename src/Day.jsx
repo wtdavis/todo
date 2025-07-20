03d6d3598f02dc624efa4f2acbf01d4eeeed428d
Will <wtdavis@wesleyan.edu>
2025-07-19 21:14:55 -0400

    test

--- src/Day.jsx
+++ src/Day.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 
 function Day () {
-    // a Day should have a day-of-the-week(dotw), 
+    // a Day should have a day-of-the-week(dotw), date, and to-do objects (?)
 
-    let dateuseState
+    let [date, changeDate] = useState()
+    let [dotw, changeDotw] = useState()
       
 
 }
