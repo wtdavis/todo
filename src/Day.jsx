d061cfdf6d3333a7715ca7c82e828f0f4d9bd90b
Will <wtdavis@wesleyan.edu>
2025-07-19 21:16:57 -0400

    useState init

--- src/Day.jsx
+++ src/Day.jsx
@@ -3,9 +3,13 @@ import React, { use, useState } from "react";
 function Day () {
     // a Day should have a day-of-the-week(dotw), date, and to-do objects (?)
 
-    let [date, changeDate] = useState()
-    let [dotw, changeDotw] = useState()
-      
+    let [date, setDate] = useState()
+    let [dotw, setDotw] = useState()
+    let [toDoObj, setToDoObj] = useState({})
+
+    return (
+
+    )
 
 }
 
