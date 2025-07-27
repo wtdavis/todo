1f2de6efcfeeafbffe51affc313e3c8009879f67
Will <wtdavis@wesleyan.edu>
2025-07-26 21:28:39 -0400

    calendar updates

--- src/Calendar/Week.jsx
+++ src/Calendar/Week.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
-
-
+import "./Day.jsx"
 function Week () { 
     let [dayList, setDayList] = useState([])
 
-    const dayNodeList = function () {
-        return (
-            <ul>
-                <li>
-                    
-                </li>
-            </ul>
-        )
-    }
+    // const dayNodeList = function () {
+    //     return (
+    //         <ul>
+    //             <li>
+
+    //             </li>
+    //         </ul>
+    //     )
+    // }
+
+    const dotwList = []
 
     return (
 
